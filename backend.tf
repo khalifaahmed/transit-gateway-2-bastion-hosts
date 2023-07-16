0315b6c62a7573b617a824a3e629d2c5b57a7bd6
khalifaahmed <aboeleefkhalifa@gmail.com>
2023-07-16 07:17:41 +0300

    local state file man

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
-  backend "s3" {
-    bucket = "grad--proj--bucket"
-    key    = "Terraform/general-project/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+#   backend "s3" {
+#     bucket = "grad--proj--bucket"
+#     key    = "Terraform/general-project/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
 
 provider "aws" {
   # determine the region in which we deploy our infrastructure
