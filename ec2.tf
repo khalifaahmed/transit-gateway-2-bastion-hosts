30a94ba66a1087041d07019bc93d2dad45227881
khalifaahmed <aboeleefkhalifa@gmail.com>
2023-07-23 23:14:20 +0300

    final transit gateway

--- ec2.tf
+++ ec2.tf
@@ -20,7 +20,6 @@ data "aws_ami" "amzlinux2" {
   }
 }
 
-
 # EC2 Instance
 resource "aws_instance" "myec2" {
   ami                         = data.aws_ami.amzlinux2.id
@@ -34,6 +33,19 @@ resource "aws_instance" "myec2" {
     "Name" = "${var.name}_ec2"
   }
 }
+resource "aws_instance" "myec2_public" {
+  ami                         = data.aws_ami.amzlinux2.id
+  instance_type               = var.instance_type
+  user_data                   = file("${path.module}/app1-install.sh")
+  key_name                    = "terraform_key"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.public[0].id
+  vpc_security_group_ids      = [aws_security_group.grad_proj_sg["ssh"].id, aws_security_group.grad_proj_sg["http_https"].id, aws_security_group.grad_proj_sg["public"].id] 
+  tags = {
+    "Name" = "${var.name}_public_ec2"
+  }
+}
+
 
 # EC2 Instance
 resource "aws_instance" "main" {
@@ -48,114 +60,17 @@ resource "aws_instance" "main" {
     "Name" = "main_ec2"
   }
 }
-
-# # EC2 Instance
-# resource "aws_instance" "myec2" {
-#   ami                         = data.aws_ami.amzlinux2.id
-#   instance_type               = var.instance_type
-#   user_data                   = file("${path.module}/app1-install.sh")
-#   key_name                    = var.key
-#   associate_public_ip_address = true
-#   subnet_id                   = aws_subnet.public[0].id
-#   vpc_security_group_ids      = [aws_security_group.grad_proj_sg["ssh"].id, aws_security_group.grad_proj_sg["http_https"].id, aws_security_group.grad_proj_sg["public"].id] #vpc_security_group_ids      = [ aws_security_group.http-only.id, aws_security_group.ssh-only.id ]
-#   count                       = var.ec2_count
-#   tags = {
-#     "Name" = "${var.name}_ec2"
-#   }
-
-#   provisioner "local-exec" {
-#     working_dir = "./ec2-docker"
-#     command = "ansible-playbook --inventory ${self.public_ip}, --user ec2-user --private-key terraform_key_pair.pem  deploy-docker.yaml"
-#   }
-# }
-
-# output "the_pubic_ip" {
-#   value = (var.ec2_count > 0) ? aws_instance.myec2[0].public_ip : ""
-# }
-
-# resource "aws_ami_from_instance" "example" {
-#   name               = "grad-proj-AMI"
-#   source_instance_id = "${aws_instance.myec2[0].id}"
-#   provisioner "local-exec" {
-#     command = "aws ec2 terminate-instances --instance-ids ${aws_instance.myec2[0].id} --region ${var.provider_region}"
-#   }
-# }
-
-data "aws_ami" "ubuntu_ami" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
+resource "aws_instance" "main_public" {
+  ami                         = data.aws_ami.amzlinux2.id
+  instance_type               = var.instance_type
+  user_data                   = file("${path.module}/app1-install.sh")
+  key_name                    = "terraform_key"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.main_public[0].id
+  vpc_security_group_ids      = [aws_security_group.main.id]
+  tags = {
+    "Name" = "main_public_ec2"
   }
 }
 
-# resource "aws_instance" "nexus" {
-#   ami                         = data.aws_ami.ubuntu_ami.id
-#   instance_type               = "t3.small"
-#   #user_data                   = file("${path.module}/my-script.sh")
-#   key_name                    = var.key
-#   associate_public_ip_address = true
-#   subnet_id                   = aws_subnet.public[0].id
-#   vpc_security_group_ids      = [aws_security_group.grad_proj_sg["ssh"].id, aws_security_group.grad_proj_sg["http_https"].id, aws_security_group.grad_proj_sg["public"].id] 
-#   tags = {
-#     "Name" = "nexus-server"
-#   }
 
-#   provisioner "local-exec" {
-#     working_dir = "./nexus"
-#     command = "ansible-playbook --inventory ${self.public_ip}, --user ubuntu --private-key /home/ahmed/Desktop/ansible/ec2-docker/terraform_key_pair.pem  deploy-nexus.yaml"
-#   }
-# }
-
-# resource "aws_instance" "httpd_server" {
-#   ami                         = "ami-02b8534ff4b424939"    #data.aws_ami.ubuntu_ami.id
-#   instance_type               = var.instance_type
-#   #user_data                   = file("${path.module}/my-script.sh")
-#   key_name                    = var.key
-#   associate_public_ip_address = true
-#   subnet_id                   = aws_subnet.public[0].id
-#   vpc_security_group_ids      = [aws_security_group.grad_proj_sg["ssh"].id, aws_security_group.grad_proj_sg["http_https"].id, aws_security_group.grad_proj_sg["public"].id] #vpc_security_group_ids      = [ aws_security_group.http-only.id, aws_security_group.ssh-only.id ]
-#   tags = {
-#     "Name" = "httpd-server"
-#   }
-
-#   provisioner "local-exec" {
-#     working_dir = "./redhat"
-#     command = "ansible-playbook --inventory ${self.public_ip}, --user ec2-user --private-key /home/ahmed/Desktop/ansible/ec2-docker/terraform_key_pair.pem  deploy_httpd.yaml"
-#     #command = "export cont_name""=${self.public_ip} ; envsubst < docker-compose.yaml > outofenvsubst2"
-#     #command = "export cont_name=${data.aws_ami.ubuntu_ami.id} ; envsubst < docker-compose.yaml > outofenvsubst3"
-#     #command = "envsubst < docker-compose.yaml > outofenvsubst2"
-#     #command = "ansible-playbook --inventory ${self.public_ip}, --user ubuntu --private-key terraform_key_pair.pem  deploy-nexus.yaml"
-#   }
-# }
-
-# resource "aws_instance" "db_server" {
-#   ami                         = "ami-02b8534ff4b424939"    #data.aws_ami.ubuntu_ami.id
-#   instance_type               = var.instance_type
-#   #user_data                   = file("${path.module}/my-script.sh")
-#   key_name                    = var.key
-#   associate_public_ip_address = true
-#   subnet_id                   = aws_subnet.public[0].id
-#   vpc_security_group_ids      = [aws_security_group.grad_proj_sg["ssh"].id, aws_security_group.grad_proj_sg["http_https"].id, aws_security_group.grad_proj_sg["public"].id] #vpc_security_group_ids      = [ aws_security_group.http-only.id, aws_security_group.ssh-only.id ]
-#   tags = {
-#     "Name" = "db-server"
-#   }
-
-#   provisioner "local-exec" {
-#     working_dir = "./redhat"
-#     command = "ansible-playbook --inventory ${self.public_ip}, --user ec2-user --private-key terraform_key_pair.pem  deploy_mysql_server.yaml"
-#   }
-# }
